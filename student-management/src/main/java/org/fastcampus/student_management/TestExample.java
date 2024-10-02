0accfaa107356e46e6c3b5721c23bf8c83c2f91f
PastelQ <a0941@naver.com>
2024-10-02 17:18:20 +0900

    2024/10/02/test/테스트 교육 전(작성하는 이유)
    # 테스트를 작성하는 이유
    - **안정성**
    1. 기능 변경이 있을 때에 영향도 파악 가능
    2. 리팩토링 시 기능의 안정성
    3. 기능의 의도를 명확하게 파악이 가능
    
    ## 테스트를 작성하지 않는 이유(어려운 이유)
    1. 의도를 전혀 모르겠는 복잡한 테스트
    2. 시간이 지나면서 80%의 Test가 통과 안되는 코드
    3. 변경할 때에 오히려 짐이 되는 Test
    
    ## 테스트의 2가지 분류
    1. 인수 테스트(통합 테스트)
    - 사용자 관점에서 요구사항을 만족하는 지 검증(최종 단계 검증)
    - 네트워크, DB 등 외부에 의존하는 것은 대부분 인수 테스트
    
    2. 단위 테스트
    - 테스트가 가능한 가장 작은 단위를 검증하는 테스트
    
    ## 인수 테스트(통합 테스트)의 한계
    1. 프로젝트를 설계하는 단계에서 빠르게 시작하기 어려움
    2. 라이브러리들을 추가해야 함
    3. 템플릿을 제대로 만든 후 진행하지 않을 경우 유지 보수가 상대적으로 어려움
    - **주의add .* 인수 테스트가 나쁜 것은 아니고 상황에 따라 다른 것 뿐!
    
    ## 단위 테스트의 조건
    - 객체 지향으로 작게 나눈 순수한 자바 객체를 테스트 진행
    
    ## 본격적인 테스트 시작 전에 앞서 알아야 할 것
    - **Given-When-Then Patten** : 테스트의 조건이 명시된 패턴
    1. Given - 주어진 상태
    2. When - 조건이 가해진 상태
    3. Then - 결과
    - 패턴을 통해 유지 보수 및 가독성 증가 (하단 예시)
    
    <p>
    @Test
    void givenFeels1000WhenChangeFee2000ThenFeeShouldBe2000(){
      // Given : 주어진 상태
      CourseFee courseFee = new CourseFee(1000);
    
      // When : 조건이 가해진 상태
      courseFee.changeFee(2000);
    
      // Then : 결과
      assertEquals(2000, courseFee.getFee());
    </p>
    
    - **테스트 더블** : 테스트를 위한 가짜 객체
    1. Stub : 어떤 동작을 요청하던, 항상 미리 준비해 둔 값만 반환
    2. Fake : 동작을 하는 것처럼 보이게 만든 객체, 단순하게 된 동작을 구현
    - Ex) InMemory / DB
    3. Mock : 테스트 환경에서의 응답을 모방할 때 사용
    - Stub와 유사하나 행위 검증 시 주로 사용됨
    - Ex) 테스트 대상 코드가 다른 컴포넌트와 예상대로 상호 작용하는 지 확인하는 용도
    
    ## 테스트는 무적이 아니다...
    - TDD : 테스트 주도 개발(선 테스트 후 개발 방식의 프로그래밍 방법)
    - TDD와 Test는 강력한 도구가 될 수 있으나 항상 그런 것은 아님
    - Test는 좋은 도구일 뿐, 아래 사항을 고려하는 것이 더 우선시 될 수 있음
    1. Test를 짜는 본인의 실력
    2. 현재 진행하는 프로젝트의 목표
    3. 팀원들의 Test에 대한 이해도 등등..
    
    - **문제 해결의 도구**로 Test를 이용하자 !

---
+++ student-management/src/main/java/org/fastcampus/student_management/TestExample.java
@@ -0,0 +1,19 @@
+package org.fastcampus.student_management;
+
+public class TestExample {
+  /* Given-When-Then Pattern이 적용된 테스트 예시
+
+  @Test // 해당 메서드명은 단위 테스트 내용을 담고 있음
+  void givenFeels1000WhenChangeFee2000ThenFeeShouldBe2000() {
+    
+    // Given : 주어진 상태
+    CourseFee courseFee = new CourseFee(1000);
+    
+    // When : 조건이 가해진 상태
+    courseFee.changeFee(2000);
+    
+    // Then : 결과
+    assertEquals(2000,courseFee.getFee());
+  }
+  */
+}
--- student-management/src/main/java/org/fastcampus/student_management/domain/CourseList.java
+++ student-management/src/main/java/org/fastcampus/student_management/domain/CourseList.java
@@ -6,7 +6,7 @@ import java.util.List;
 - 1급 컬렉션을 활용할 경우 장점
 1. 로직을 쉽게 파악하기 쉬움(메서드 명만으로 쉽게 로직을 파악하기 쉬움)
 2. 재사용이 가능
-3. Test가 쉬워짐 */
+3. Test가 쉬워짐 : Service 레이어의 종속성을 구현하지 않아도 됨 */
 public class CourseList {
 
 	private List<Course> courses;
